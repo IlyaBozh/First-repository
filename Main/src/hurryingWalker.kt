f697a0defa6d7571eaca25a5fccd3c2ff6bc2866
IlyaBozh <86633291+IlyaBozh@users.noreply.github.com>
2021-07-21 19:28:25 +0300

    Revert "Add calss SadWalker"

--- Main/src/hurryingWalker.kt
+++
@@ -1,5 +0,0 @@
-class HurryingWalker(override val speed: Int) : IWalker{
-    override fun walk(){
-        println("this man is in a hurry somewhere, he has a very high speed, about $speed km/h")
-    }
-}
--- Main/src/main.kt
+++ Main/src/main.kt
@@ -1,6 +1,4 @@
 fun main(){
     val human = Speed(12)
     human.showSpeed()
-    val hurryingHuman: IWalker = HurryingWalker(7)
-    hurryingHuman.walk()
 }
