4f2aea37df203ac20ada891bdb91c7898717ca3c
Ilya Bozhkov <ilia.il.27@mail.ru>
2021-07-21 21:48:50 +0300

    Remov the variable declaration in the interface and the var redefinition

--- Main/src/sadWalker.kt
+++ Main/src/sadWalker.kt
@@ -1,4 +1,4 @@
-class SadWalker(override val speed: Int) : IWalker{
+data class SadWalker(val speed: Int = 0) : IWalker{
     override fun walk(){
         println("He is so sad, that's probably why he goes about $speed km/h")
     }
